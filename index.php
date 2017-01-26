861af6c166614b65a97ccd45429a9104c40008d5
Camilo Riviere <SameThingButCheaper@SameThingButCheaper.com>
2017-01-26 15:02:27 -0500

    index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 $data = file_get_contents('https://www.codeschool.com/users/Criviere.json');
 $json_data = json_decode($data, true);
-var_dump($json_data);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title><?php echo "$json_data['user']['username']" ?></title>
 </head>
 <body>
 
