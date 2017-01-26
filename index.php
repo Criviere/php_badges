6b9134023b2c498560e321bea8fbf1c3b267c14d
Camilo Riviere <SameThingButCheaper@SameThingButCheaper.com>
2017-01-26 15:45:35 -0500

    index.php modified

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 $data = file_get_contents('https://www.codeschool.com/users/Criviere.json');
 $json_data = json_decode($data, true);
-// var_dump($json_data['courses']['completed']);
+var_dump($json_data['courses']['completed']);
 $courses = $json_data['courses']['completed'];
 ?>
 <!DOCTYPE html>
@@ -12,12 +12,12 @@ $courses = $json_data['courses']['completed'];
   <title><?php echo $json_data['user']['username']; ?>'s Badges</title>
 </head>
 <body>
-  <?php
+  <!-- <?php
   foreach ($courses as $course) {
     echo '<div>';
     echo '<a href="' . $course["url"] . '">'. $course["title"] . '</a>';
     echo '</div>';
   }
-   ?>
+   ?> -->
 </body>
 </html>
